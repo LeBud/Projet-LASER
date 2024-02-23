e44e73aa70e6e4d154c90147c3dbae10a7e86adc
LeBud <c.huyghe@rubika-edu.com>
2024-02-23 14:49:08 +0100

    SEconde LD

--- Hoverboard/Assets/Scripts/SwitchScene.cs
+++ Hoverboard/Assets/Scripts/SwitchScene.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int nextSceneIndex;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
     }
 }
